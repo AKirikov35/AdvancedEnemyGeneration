ae16df2c27bcdbb60de0fcea4c895474de3b49d0
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-11-13 12:07:35 +0300

    BlueCubeFactory.cs

--- BlueCubeFactory.cs
+++ BlueCubeFactory.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
 
-public class BlueCubeFactory : MonoBehaviour, IEnemyFactory
+public class BlueCubeFactory : EnemyFactory
 {
     [SerializeField] private BlueCube _prefab;
 
-    private Transform _target;
-
-    public Enemy CreateEnemy(Transform spawnPoint)
+    private void Awake()
     {
-        BlueCube enemy = Instantiate(_prefab, spawnPoint.position, Quaternion.identity);
-        enemy.Init(_target);
-
-        return enemy;
-    }
-
-    public void GetTarget(Transform target)
-    {
-        _target = target;
+        Prefab = _prefab;
     }
 }
