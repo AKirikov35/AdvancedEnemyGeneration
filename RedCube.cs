9b091f7e114f77343b55acfbcf936b4bd88357cd
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-11-13 12:08:55 +0300

    Update RedCube.cs

--- RedCube.cs
+++ RedCube.cs
@@ -1,30 +1,3 @@
-using System;
-using UnityEngine;
-
 public class RedCube : Enemy
 {
-    public override event Action<Enemy> HasDestroy;
-
-    private void Update()
-    {
-        Move();
-    }
-
-    public override void Init(Transform target)
-    {
-        _target = target;
-    }
-
-    protected override void Move()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.TryGetComponent<Target>(out _))
-        {
-            HasDestroy?.Invoke(this);
-        }
-    }
 }
